78a6f1e9cef8e5f8433f88e4db20dc362b944132
sha-lyn <shawneelynn@outlook.com>
2025-07-25 15:28:09 +0000

    changes

--- ui.R
+++ ui.R
@@ -1,1 +1,16 @@
+library(shiny)
+library(bslib)
+library(bsicons)
+library(shinyWidgets)
 
+ui <- page_sidebar(
+  title = "Stardew Valley Crop Calculator",
+  sidebar = sidebar(
+    "This is where the user input widgets will go."
+  ),
+  card(
+    card_header("Profits"),
+    "This section will display all of the calculations.",
+    card_footer("Stardew Valley Crop Calculator"),
+  )
+)
